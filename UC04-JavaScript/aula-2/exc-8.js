9d1b5c20f6ac013bd0d87b8c1e2278e068b9cae8
nathansantos07 <nathan.oliver07@hotmail.com>
2025-08-22 18:49:21 -0300

    docs: att

--- UC04-JavaScript/aula-2/exc-8.js
+++ UC04-JavaScript/aula-2/exc-8.js
@@ -10,11 +10,13 @@ export default function(){
         
         if(Number.isInteger(num) && (num >= 0 && num < 1000 )){
             centenas = Math.floor(num/100) // jogando a casa decimal para trás em duas casas e descarto DEZENA E UNIDADE
-                console.log(`\n${centenas} centenas`);
+                if(centenas > 0){
+                    console.log(`\n${centenas} centenas`);}
 
             let unid_fora = Math.floor(num/10) // o mesmo passo mas agora isolando só o ultimo numero e descartando a UNIDADE
             dezenas = unid_fora % 10 // deixando só o ultimo numero
-                console.log(`${dezenas} dezenas`);
+                if(dezenas > 0){
+                    console.log(`${dezenas} dezenas`);}
 
             unidades = num%10 // o resto sempre gera o último numero, ou seja, a unidade
                 console.log(`${unidades} unidades`);            
