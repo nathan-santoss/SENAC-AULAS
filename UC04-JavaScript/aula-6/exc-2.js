88736f1ba58a0a25997e514a41c2ef24e2390d81
nathansantos07 <nathan.oliver07@hotmail.com>
2025-09-04 17:48:21 -0300

    :books: melhorias no código

--- UC04-JavaScript/aula-6/exc-2.js
+++ UC04-JavaScript/aula-6/exc-2.js
@@ -1,86 +1,75 @@
 import promptSync from "prompt-sync";
 const prompt = promptSync()
 
-class roupas {
-    constructor(){
-        this.item
-        this.quantidade
+class roupas { //criando a classe do objeto 
+    constructor(item, quantidade){
+        this.item = item.toUpperCase() // informe as propriedades que serão inseridas
+        this.quantidade = quantidade
     }
 }
-let catalogo_1 = []
+let catalogo_1 = [] // criando os dois catálogos
 let catalogo_2 = []
 
-while(catalogo_1.length < 10 || catalogo_2.length < 10){
+while(catalogo_1.length < 10 || catalogo_2.length < 10){ // estrutura de repetição: enquanto os catalogos possuem menos que 10 indices
     console.log(`
         [----- CADASTRO DE ITENS -----]\n`);
     let nome = prompt('Informe o nome da roupa: ').toUpperCase()
     let qtde = Number(prompt('Quantos itens deste estão disponíveis? '))
-    if(nome.trim() == '' || qtde < 0 || isNaN(qtde) || !Number.isInteger(qtde)){
+    if(nome.trim() == '' || qtde < 0 || isNaN(qtde) || !Number.isInteger(qtde)){ // teste para prosseguir 
         console.log('INFORMAÇÕES INVÁLIDAS!\n');
     }
     else{
-        let roupa = new roupas()
-        if(catalogo_1.length < 10){
-            roupa.item = nome
-            roupa.quantidade = qtde
-
-            catalogo_1.push(roupa)
+        let roupa = new roupas(nome, quantidade) // objeto criado
+        if(catalogo_1.length < 10){ // teste para saber se o primeiro catálogo está totalmente preenchido
+            catalogo_1.push(roupa) // se sim, puxa a roupa pra dentro do catálogo
         }
-        else if(catalogo_1.length == 10){
-            roupa.item = nome
-            roupa.quantidade = qtde
-
-            catalogo_2.push(roupa)
+        else{
+            catalogo_2.push(roupa) // se nao, puxa ela pro catálogo 2
         }
         console.clear()
     }
 }
-catalogo_1.splice(4, 3)
-let loja = [...catalogo_1,...catalogo_2]
+catalogo_1.splice(4, 3) // removendo 3 itens a partir do indice 4 
+let loja = [...catalogo_1,...catalogo_2] // operador spread espalha o array 
 
-let flag = true
-while(flag){
-    let buscar_item = prompt('Informe o nome da peça para buscar --> ').toUpperCase()
-    let existe = false
-    for(let i = 0; i < loja.length; i++){
-        if(buscar_item === loja[i].item){
-            existe = true
-            let disp
-            if(loja[i].quantidade > 0){disp = `Unidades disponíveis --> ${loja[i].quantidade}`}
-            else{disp = `Sem disponibilidade no momento!`}
-
+let flag = true // flag para repetição
+while(flag){ // enquanto for verdadeira
+    let buscar_item = prompt('Informe o nome da peça para buscar --> ').toUpperCase() // recebo o nome do item
+    let roupa = loja.find(obj => obj.item === buscar_item) // find busca dentro do array
+    // 'obj' é o apelido que dei aos itens do array, como é um objeto, tenho que usar '.propriedadeDoObj' 
+        if(roupa){ // estrutura para saber se o item existe
+            // o if entende que todo valor existente tem um valor true, diferente de 'null', 'NaN' e 'undefined' onde retorna 'false'
+            let disp // variavel para guardar a disponibilidade
+            if(roupa.quantidade > 0){ // se tenho pelo menos um item
+                disp = `Unidades disponíveis --> ${roupa.quantidade}`// a disp recebe o número de itens
+            }
+            else{
+                disp = `Sem disponibilidade no momento!`
+            }
             console.log(`
                 [--- ITEM DISPONÍVEL ---]\n
-                NOME --> ${loja[i].item}\n
-                POSIÇÃO DO ARRAY --> ${i}\n
+                NOME --> ${roupa.item}\n
+                POSIÇÃO DO ARRAY --> ${loja.indexOf(roupa)}\n
                 ${disp}`
             );
             break
         }
-    }
-    if(existe == false){
+    if(!roupa){ // se o item não existe
         console.log(`
             [--- ITEM INDISPONÍVEL ---]\n`
         );
     }
-
-    let decision
     do{
         console.log(`
             \nDeseja uma nova busca?\n
             [1]SIM   [2]NÃO\n`
         );
-        decision = Number(prompt('---> '))
-        switch(decision){
-            case 1:
-                break
-            case 2:
-                flag = false
-                break
-            default:
-                console.log(`OPÇÃO INVÁLIDA! \n`);
+        let decision = Number(prompt('---> '))
+        if (decision === 2) {
+            flag = false;
+        } 
+        else if (decision !== 1) {
+            console.log("OPÇÃO INVÁLIDA!\n");
         }
     }while(decision != 1 && decision != 2)
 }
-
-
