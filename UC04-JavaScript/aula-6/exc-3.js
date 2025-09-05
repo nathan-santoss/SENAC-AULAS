ffdb246e25598c4d68c34fa9423289e9c00a94cc
nathansantos07 <nathan.oliver07@hotmail.com>
2025-09-05 01:23:20 -0300

    :books: docs: atividade 3 entregue

--- UC04-JavaScript/aula-6/exc-3.js
+++ UC04-JavaScript/aula-6/exc-3.js
@@ -1,3 +1,51 @@
 import promptSync from "prompt-sync";
 const prompt = promptSync()
 
+let flag = true
+let chat = []
+do{
+    let mensagem = prompt('digite aqui --> ').toUpperCase()
+    if(mensagem.trim() == ''){
+        chat.unshift(mensagem)
+        flag = false
+    }
+}while(flag)
+
+let ultimas = chat.slice(-10).reverse()
+let recentes = chat.slice(0, 10)
+
+console.log(`
+    [--- 10 ÚLTIMAS ---]\n
+    --> ${ultimas.join("\n")}`
+);
+console.log(`
+    [--- 10 RECENTES ---]\n
+    --> ${recentes.join("\n")}`
+);
+
+flag = true
+while(flag){
+    let palavra = prompt('Informe a mensagem a palavra chave --> ').toUpperCase()
+    if(palavra.trim() == ''){console.log('O CAMPO NÃO PODE ESTÁ VAZIO!\n');}
+    else{
+        let search = chat.filter((mensagem) => {
+            return mensagem.includes(palavra)
+        })
+        if(search.length == 0){console.log(`MENSAGEM NÃO ENCONTRADA\n`);}
+        else{
+            console.log(`
+            [---- MENSAGENS ENCONTRADAS ----]\n
+                ${search.join('\n')}`
+            );
+            
+            let decision
+            do{
+                console.log(`
+                    Deseja realizar uma nova busca?
+                    [1] SIM ou [2] NÃO`);
+                decision = Number(prompt('---> '))
+                if(decision == 2){flag = false}              
+            }while(decision != 1 && decision != 2)
+        }
+    }
+}
