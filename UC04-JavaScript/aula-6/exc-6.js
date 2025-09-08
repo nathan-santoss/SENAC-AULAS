6f5fcdc208c4f38c4d4b66319991410b9d3a6d67
nathansantos07 <nathan.oliver07@hotmail.com>
2025-09-08 18:59:35 -0300

    :books: docs: comentários

--- UC04-JavaScript/aula-6/exc-6.js
+++ UC04-JavaScript/aula-6/exc-6.js
@@ -12,17 +12,18 @@ const senhas = [
     'VALOR!',
     'Ac3sso@'
 ];
+console.log(senhas);
 const especiais = ['@', '#', '!', '%']
-senhas.forEach((senha, i) => {
-    let teste = senha.split('')
-    let temNumero = teste.some(caract => !isNaN(caract))
-    let temMAIUSCULA = teste.some(caract => caract !== caract.toLowerCase())
-    let temEspecial = teste.some(caract => especiais.includes(caract))
+senhas.forEach((senha, i) => { // percorro o array de senhas e meu paramentro inicial é cada 'SENHA' e sei indice (i)
+    let teste = senha.split('') // 'teste' irá receber o um array da 'senha' atual fracionada
+    let temNumero = teste.some(caract => !isNaN(caract)) // peço para percorrer cada caracter e ver se tem algum número (retorna booleano)
+    let temMAIUSCULA = teste.some(caract => caract !== caract.toLowerCase()) // percorro o array e teste se existe algum caracter diferente dele mesmo em minusculo
+    let temEspecial = teste.some(caract => especiais.includes(caract)) // percorro o array e vejo se algum dos caracteres especiais fazem parte desse caracter 
     if(temNumero == true && temMAIUSCULA == true && temEspecial == true){
-        senhas[i] = `${senha} = SENHA FORTE <--`
-    }
+        senhas[i] = `${senha} = SENHA FORTE <--` // se todos forem verdade, a senha atende os critérios
+    }  
     else{
-        senhas[i] = `${senha} = SENHA FRACA`
+        senhas[i] = `${senha} = SENHA FRACA (X)`
     }
 });
 
