9d4c0582310c7097539688d88f326f0718b19c03
nathansantos07 <nathan.oliver07@hotmail.com>
2025-09-10 00:07:01 -0300

    :books: docs: exc-2 feito

--- UC04-JavaScript/aula-7/exc-2.js
+++ UC04-JavaScript/aula-7/exc-2.js
@@ -2,25 +2,46 @@ import promptSync from 'prompt-sync'
 const prompt = promptSync()
 
 let flag = true
-let saldoatual = 0
+let saldo = 0
+let valor
+
+let sacar = (valor, saldo) => {
+    saldo -= valor
+    return saldo
+}
+let depositar = (valor, saldo) => {
+    saldo += valor
+    return saldo
+}
+
 do{
     console.log(`
-        [1] SACAR ou [2] DEPOSITAR`);
-    let result
-    let operacao
+        [1] SACAR
+        [2] DEPOSITAR 
+        [3] SAIR`);
     let option = Number(prompt('---> '))
 
     switch(option){
         case 1:
-            operacao = Number(prompt('Informe qual vai ser o SAQUE em REAIS -> ')).toFixed(2)
-            result = sacar(valor,saldo)
+            valor = Number(prompt('Informe qual vai ser o SAQUE em REAIS -> ')).toFixed(2)
+            saldo = sacar(valor,saldo)
+            console.clear()
+            console.log(`\nSeu saldo atual é = R$${saldo.toFixed(2)}`);
             break
         case 2:
-            operacao = Number(prompt('Informe o valor do DEPÓSITO em REAIS -> '))
-            depositar
+            valor = Number(prompt('Informe o valor do DEPÓSITO em REAIS -> '))
+            saldo = depositar(valor,saldo)
+            console.clear()
+            console.log(`Seu saldo atual é = R$${saldo.toFixed(2)}`);
+            break
+        case 3:
+            flag = false
+            break
+        default:
+            console.log(`${option} NÃO É UMA OPÇÃO VÁLIDA`);
+            break
     }
 }while(flag)
 
 
-const sacar = (valor, saldo) => saldo-valor
-const depositar = (valor, saldo) => saldo+valor
+
