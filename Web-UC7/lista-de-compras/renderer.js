ae8afb68383a37ccac08a09365d70259af95ec39
nathan-santoss <nathan.oliver07@hotmail.com>
2026-04-03 21:06:12 -0300

    :books: att

--- Web-UC7/lista-de-compras/renderer.js
+++ Web-UC7/lista-de-compras/renderer.js
@@ -2,8 +2,9 @@
 const add_bt = document.getElementById('botao-add')
 
 add_bt.addEventListener('click', (event) => {
-    const produto = document.getElementById('nome-prod').value
-    adicionar_produto(produto)
+    const produto = document.getElementById('nome-prod')
+    adicionar_produto(produto.value)
+    produto.value = ''
 })
 
 
@@ -11,26 +12,30 @@ const adicionar_produto = (produto) => {
     const categoria = document.getElementById('categoria').value
     const lista_destino = document.getElementById(`lista-${categoria.toLowerCase()}`)
     const new_item = document.createElement('li')
-    
-    const qtde_itens = lista_destino.children.length
     const qtde_text = lista_destino.querySelector('p')
-    
 
-    switch(categoria.toLowerCase()){
-        case 'limpeza':
-            new_item.innerHTML = `${produto.toUpperCase()}`
-            break
-        case 'frutas':
-            new_item.innerHTML = `${produto.toUpperCase()}`
-            break
-        case 'mercearia':
-            new_item.innerHTML = `${produto.toUpperCase()}`
-            break
-        case 'congelados':
-            new_item.innerHTML = `${produto.toUpperCase()}`
-            break
-    }
+    new_item.innerHTML = `
+        ${produto.toUpperCase()}
+        <button class="btn-feito">✔️</button>
+        <button class="btn-delete">🗑️</button>
+    `
+
+
     lista_destino.appendChild(new_item)
+    const qtde_itens = lista_destino.querySelectorAll('li').length
     qtde_text.innerHTML = `(${qtde_itens})`
     lista_destino.style.display = 'block'
+
+    const btn_deleteItem = new_item.querySelector('.btn-delete')
+    btn_deleteItem.addEventListener('click', (event) => {
+        new_item.remove()
+        let novototal = lista_destino.querySelectorAll('li').length
+        qtde_text.innerHTML = `(${novototal})`
+    })
+
+    const btn_finishItem = new_item.querySelector('.btn-feito')
+    btn_finishItem.addEventListener('click', (event) => {
+        new_item.style.textDecoration = 'line-through'
+    })
+
 }
